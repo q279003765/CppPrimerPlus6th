1040cdb2b8b0ec987fb5d12dff8736629c31d1a6
xxin <xxin225@163.com>
2020-11-05 12:45:52 +0800

    c++继承

---
+++ CppPrimerPlus6th/list_13_7_brass.cpp
@@ -0,0 +1,9 @@
+//
+//  list_13_7_brass.cpp
+//  CppPrimerPlus6th
+//
+//  Created by xxin on 2020/11/4.
+//  Copyright © 2020 xxin. All rights reserved.
+//
+
+#include "list_13_7_brass.hpp"
---
+++ CppPrimerPlus6th/list_13_7_brass.h
@@ -0,0 +1,14 @@
+//
+//  list_13_7_brass.hpp
+//  CppPrimerPlus6th
+//
+//  Created by xxin on 2020/11/4.
+//  Copyright © 2020 xxin. All rights reserved.
+//
+
+#ifndef list_13_7_brass_hpp
+#define list_13_7_brass_hpp
+
+#include <stdio.h>
+
+#endif /* list_13_7_brass_hpp */
